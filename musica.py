fc4f9fe09ae78843b7386c6d3e850bae5af10a6b
Daniel Xavier <daniel.xds@hotmail.com>
2024-01-06 09:35:46 -0300

    criando rota para chamar a página html

--- musica.py
+++ musica.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 def hello():
     return "<h3>Bem vindo a minha primeira aplicação flask</h3>"
 
+@app.route('/musicas')
+def listarMusicas():
+    return render_template('lista_musicas.html')
+
 app.run()
