7dbd130cb3ecca086192b6d86e69f2e0407889d2
Daniel Xavier <daniel.xds@hotmail.com>
2024-01-24 23:44:04 -0300

    inserindo mais de uma musica na tabela

--- comandos_banco_de_dados.sql
+++ comandos_banco_de_dados.sql
@@ -22,7 +22,12 @@ select nome_musica, genero_musica from musica;
 insert into musica(nome_musica, cantor_banda, genero_musica)
 values('O sol', 'Vitor Kley', 'Pop');
 
-
+insert into musica(nome_musica, cantor_banda, genero_musica)
+values('Cavalo de Troia', 'Mc Kelvin', 'Funk'),
+('Isis', 'Mc Kako', 'Funk'),
+('Pai é quem cria', 'Tierry', 'Sertanejjo'),
+('Lobo Guará', 'Hungria', 'Rep'),
+('Meu abrigo', 'Mellin', 'Popp');
 
 
 
