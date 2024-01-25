0659bd73a5c36670984a4d44432bbcbeccd349ca
Daniel Xavier <daniel.xds@hotmail.com>
2024-01-24 23:41:36 -0300

    criando tabela de musica

--- comandos_banco_de_dados.sql
+++ comandos_banco_de_dados.sql
@@ -2,6 +2,13 @@ create database playMusica;
 
 use playmusica;
 
+create table musica(
+	id_musica int primary key auto_increment not null,
+    nome_musica varchar(50) not null,
+    cantor_banda varchar(50) not null,
+    genero_musica varchar(20) not null);
+    
+
 
 
 
