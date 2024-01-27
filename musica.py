3dda8e9299e655566893b564c76a1a41f803351c
Daniel Xavier <daniel.xds@hotmail.com>
2024-01-27 15:49:45 -0300

    criando a model para as classes do projeto

--- musica.py
+++ musica.py
@@ -8,23 +8,7 @@ app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 
-class Musica(db.Model):
-    id_musica = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    nome_musica = db.Column(db.String(50), nullable=False)
-    cantor_banda = db.Column(db.String(50), nullable=False)
-    genero_musica = db.Column(db.String(20), nullable=False)
 
-    def __repr__(self):
-        return '<Name %r>' %self.name
-    
-class Usuario(db.Model):
-    id_usuario = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    nome_usuario = db.Column(db.String(50), nullable=False)
-    login_usuario = db.Column(db.String(20), nullable=False)
-    senha_usuario = db.Column(db.String(15), nullable=False)
-
-    def __repr__(self):
-        return '<Name %r>' %self.name
 
 
 @app.route('/')
