6b369f7f2d32faa6a2437301bb8cba0bb545f9be
Daniel Xavier <daniel.xds@hotmail.com>
2024-02-17 23:05:00 -0300

    aplicando token no projeto

--- musica.py
+++ musica.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request, redirect, session, flash, url
 
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_wtf.csrf import CSRFProtect
+
 app = Flask(__name__)
 
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
+
+csrf = CSRFProtect(app)
 
 from views import *
 
